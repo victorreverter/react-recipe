c0794cbb7d28104e56fb7a15c32148fbe8628a4e
victorreverter <victormanuel.cuba@gmail.com>
2019-10-16 06:11:51 +0200

    Updates

---
+++ precache-manifest.1092de94ead9a674e61ef1a758177bca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0cdd9906b984a2ffc9bc14a9d7f2499",
+    "url": "/react-recipe/index.html"
+  },
+  {
+    "revision": "d9ade84487975af42dac",
+    "url": "/react-recipe/static/css/main.b42a761c.chunk.css"
+  },
+  {
+    "revision": "60c91f152ac1b401c0a0",
+    "url": "/react-recipe/static/js/2.e5611c26.chunk.js"
+  },
+  {
+    "revision": "d9ade84487975af42dac",
+    "url": "/react-recipe/static/js/main.371c3f65.chunk.js"
+  },
+  {
+    "revision": "adc3e7a999812b274bca",
+    "url": "/react-recipe/static/js/runtime-main.bbd6f274.js"
+  }
+]);
